d93af03fe738cc21e16fdf19c0bd994f5cf9a7ea
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-02-14 12:37:53 +0530

    Time: 2 ms (98.59%) | Memory: 48 MB (83.04%) - LeetSync

--- 815-champagne-tower/champagne-tower.java
+++ 815-champagne-tower/champagne-tower.java
@@ -5,16 +5,16 @@ class Solution {
             double[] curr = new double[row + 1];
 
             for(int i = 0; i < prev.length; i++){
-                double extra = prev[i] - 1.0;
+                double extra = prev[i] - 1;
                 if(extra > 0){
-                    curr[i] += extra/2.0;
-                    curr[i + 1] += extra/2.0;
+                    curr[i] += extra * 0.5;
+                    curr[i + 1] += extra * 0.5;
                 }
             }
 
             prev = curr;
         }
 
-        return Math.min(1.0, prev[query_glass]);
+        return Math.min(1, prev[query_glass]);
     }   
 }
