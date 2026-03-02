0f78b9a9f5ebefecf3e1a1f725736074992ccd5a
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-03-02 16:35:01 +0530

    Time: 4 ms (100.00%) | Memory: 12.7 MB (100.00%) - LeetSync

--- 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.py
+++ 1658-minimum-swaps-to-arrange-a-binary-grid/minimum-swaps-to-arrange-a-binary-grid.py
@@ -1,7 +1,9 @@
-class Solution:
-    def minSwaps(self, grid: List[List[int]]) -> int:
+class Solution(object):
+    def minSwaps(self, grid):
         n = len(grid)
         pos = [-1] * n
+        
+        # Find rightmost 1 in each row
         for i in range(n):
             for j in range(n - 1, -1, -1):
                 if grid[i][j] == 1:
@@ -9,19 +11,22 @@ class Solution:
                     break
         
         ans = 0
+        
         for i in range(n):
             k = -1
+            
+            # Find a row we can bring up
             for j in range(i, n):
                 if pos[j] <= i:
-
-                    ans += j - i
                     k = j
+                    ans += j - i
                     break
-
-            if k != -1:
-                for j in range(k, i, -1):
-                    pos[j], pos[j - 1] = pos[j - 1], pos[j]
-            else:
+            
+            if k == -1:
                 return -1
-        return ans
+            
+            # Bubble row k up to position i
+            for j in range(k, i, -1):
+                pos[j], pos[j - 1] = pos[j - 1], pos[j]
         
+        return ans
