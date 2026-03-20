5c4e7d96a892fdb03ce683ff42b64ce354e688d4
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-03-20 12:56:21 +0530

    Time: 47 ms (37.10%) | Memory: 19.3 MB (85.48%) - LeetSync

---
+++ 3884-minimum-absolute-difference-in-sliding-submatrix/minimum-absolute-difference-in-sliding-submatrix.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minAbsDiff(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        n = len(grid)
+        m = len(grid[0])
+        res = [[0] * (m - k + 1) for _ in range(n - k + 1)] 
+        
+        for i in range(n):
+            for j in range(m):
+                if i + k <= n and j + k <= m:
+                    curr = set()
+                    for ii in range(i, i + k):
+                        for jj in range(j, j + k):
+                            curr.add(grid[ii][jj])
+
+                    if len(curr) == 1:
+                        res[i][j] = 0
+                    else:
+                        mn = inf
+                        s = sorted(curr)
+                        for ii, v in enumerate(s):
+                            if ii == 0:
+                                continue
+                            mn = min(mn, v - s[ii - 1])
+
+                        res[i][j] = mn
+        
+        return res
