a93e779eccedfb17814d8e15c2e6f3766ab23a22
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-03-21 12:30:21 +0530

    Time: 4 ms (18.52%) | Memory: 12.6 MB (88.89%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.py
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.py
@@ -6,5 +6,5 @@ class Solution(object):
                 break
             for j in range(y, y + k):
                 grid[i][j], grid[x + k - 1 - sub_row][j] = grid[x + k - 1 - sub_row][j], grid[i][j]
-            sub_row = sub_row + 1
+            sub_row += 1
         return grid
