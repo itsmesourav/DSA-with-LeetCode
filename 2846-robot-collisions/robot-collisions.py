b9e1161457efbff9bd1da328844e369b217b0a3a
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-04-01 22:59:38 +0530

    Time: 403 ms (15.38%) | Memory: 54.3 MB (7.69%) - LeetSync

--- 2846-robot-collisions/robot-collisions.py
+++ 2846-robot-collisions/robot-collisions.py
@@ -1,47 +1,29 @@
-from typing import List
-
-class Solution:
-    def survivedRobotsHealths(self, positions: List[int], healths: List[int], directions: str) -> List[int]:
+class Solution(object):
+    def survivedRobotsHealths(self, positions, healths, directions):
         n = len(positions)
-        
-        robots = sorted([(positions[i], healths[i], directions[i], i) for i in range(n)])
-        
-        stack = []  # stores indices in 'robots' list
-        
-        # Convert to list for mutability
-        healths = list(healths)
-        
+        arr = []
+        s = []
+        for i in range(n): arr.append([i, positions[i], healths[i], directions[i]])
+        arr.sort(key = lambda x: x[1])
+
         for i in range(n):
-            pos, h, d, idx = robots[i]
-            
-            if d == 'R':
-                stack.append(i)
-            else:  # d == 'L'
-                while stack and healths[idx] > 0:
-                    j = stack[-1]
-                    _, _, _, prev_idx = robots[j]
-                    
-                    if healths[prev_idx] < healths[idx]:
-                        # R robot dies
-                        stack.pop()
-                        healths[idx] -= 1
-                        healths[prev_idx] = 0
-                    elif healths[prev_idx] > healths[idx]:
-                        # L robot dies
-                        healths[prev_idx] -= 1
-                        healths[idx] = 0
+            idx, p, h, d = arr[i]
+            if d == 'R': s.append([idx, p, h, d])
+            else:
+                while s and s[-1][3] == 'R':
+                    if s[-1][2] == h:
+                        s.pop()
+                        h = 0
+                        break
+                    elif s[-1][2] > h:
+                        s[-1][2] -= 1
+                        h = 0
                         break
                     else:
-                        # both die
-                        stack.pop()
-                        healths[prev_idx] = 0
-                        healths[idx] = 0
-                        break
-        
-        # Collect survivors in original order
-        result = []
-        for i in range(n):
-            if healths[i] > 0:
-                result.append(healths[i])
-        
-        return result
+                        s.pop()
+                        h -= 1
+                if h: s.append([idx, p, h, d])
+        s.sort(key = lambda x: x[0])
+        res = []
+        for idx, p, h, d in s: res.append(h)
+        return res
