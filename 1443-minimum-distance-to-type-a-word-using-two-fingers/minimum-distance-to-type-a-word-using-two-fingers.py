56051afbf25a45f78289be94015cb4ff74976530
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-04-12 13:56:33 +0530

    Time: 104 ms (77.97%) | Memory: 27 MB (54.24%) - LeetSync

--- 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.py
+++ 1443-minimum-distance-to-type-a-word-using-two-fingers/minimum-distance-to-type-a-word-using-two-fingers.py
@@ -14,7 +14,7 @@ class Solution:
             best = min(best, d1 + dp(si + 1, (i, j), f2))
 
             d2 = 0 if f2 == (-1, -1) else abs(f2[0] - i) + abs(f2[1] - j)
-            best = min(best, d2 + dp(si + 1, f1, (i, j)))
+            best = min(best, d2 + dp(si + 1, (i, j), f1))
 
             return best
         return dp(0, (-1, -1), (-1, -1))
