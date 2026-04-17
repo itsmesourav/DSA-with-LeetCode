300b179dcc8eb3bff1c6bde709612cfd6cad7bf1
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-04-18 00:50:02 +0530

    Time: 215 ms (73.64%) | Memory: 41.9 MB (73.64%) - LeetSync

--- 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
+++ 4139-minimum-absolute-distance-between-mirror-pairs/minimum-absolute-distance-between-mirror-pairs.py
@@ -1,12 +1,12 @@
-class Solution(object):
-    def minMirrorPairDistance(self, nums):
+class Solution:
+    def minMirrorPairDistance(self, nums: List[int]) -> int:
         rev = {}
-        res = 'inf'
+        res = inf
 
         for i, n in enumerate(nums):
             if n in rev:
                 res = min(res, i - rev[n])
             r = int(str(n)[::-1])
             rev[r] = i
-        
-        return res if res != 'inf' else -1
+
+        return res if res != inf else -1
