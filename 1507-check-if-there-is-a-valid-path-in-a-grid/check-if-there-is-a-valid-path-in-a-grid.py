a6142e55cae20cd5fffd6085fae2648e145ef3cc
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-04-27 11:04:30 +0530

    Time: 488 ms (21.88%) | Memory: 129.4 MB (6.25%) - LeetSync

---
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def hasValidPath(self, grid):
+        dirs = {
+            1: [(0, 1), (0, -1)],
+            2: [(-1, 0), (1, 0)],
+            3: [(0, -1), (1, 0)],
+            4: [(0, 1), (1, 0)],
+            5: [(-1, 0), (0, -1)],
+            6: [(-1, 0), (0, 1)]
+        }
+
+        opposite = {
+            (1, 0): (-1, 0),
+            (-1, 0): (1, 0),
+            (0, 1): (0, -1),
+            (0, -1): (0, 1)
+        }
+
+        def dfs(i, j):
+            if i == n - 1 and j == m - 1:
+                return True
+
+            for di, dj in dirs[grid[i][j]]:
+                ni, nj = i + di, j + dj
+                if ni < 0 or ni == n or nj < 0 or nj == m or (ni, nj) in seen:
+                    continue
+                if opposite[(di, dj)] in dirs[grid[ni][nj]]:
+                    seen.add((ni, nj))
+                    if dfs(ni, nj):
+                        return True
+            
+            return False
+
+        n = len(grid)
+        m = len(grid[0])
+        seen = {(0, 0)}
+        return dfs(0, 0)
+        
