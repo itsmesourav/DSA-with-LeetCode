8aae2cbe4f8a97e079a6a1e60e2bb17c476bb95b
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-05-13 22:41:28 +0530

    Time: 191 ms (75.38%) | Memory: 31.2 MB (43.08%) - LeetSync

---
+++ 1793-minimum-moves-to-make-array-complementary/minimum-moves-to-make-array-complementary.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minMoves(self, nums: List[int], limit: int) -> int:
+        n = len(nums)
+        dif = [0] * (2 * limit + 2)
+
+        for i in range(n // 2):
+            l = nums[i]
+            r = nums[n - 1 - i]
+
+            dif[min(l, r) + 1] -= 1
+            dif[l + r] -= 1
+            dif[l + r + 1] += 1
+            dif[max(l, r) + limit + 1] += 1
+        
+        res, curr = n, n
+        for i in range(2, 2 * limit + 1):
+            curr += dif[i]
+            res = min(res, curr)
+        
+        return res
