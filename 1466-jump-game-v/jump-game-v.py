f40e4d27612ff62f0a923c72db6dcb4efb55d340
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-05-24 13:39:51 +0530

    Time: 566 ms (40.00%) | Memory: 23.5 MB (40.00%) - LeetSync

--- 1466-jump-game-v/jump-game-v.py
+++ 1466-jump-game-v/jump-game-v.py
@@ -1,23 +1,32 @@
-class Solution:
-    def maxJumps(self, arr: List[int], d: int) -> int:
+class Solution(object):
+    def maxJumps(self, arr, d):
         n = len(arr)
-        @cache
+        memo = {}
+
         def dfs(i):
+            if i in memo:
+                return memo[i]
+
             res = 1
 
+            # Right
             for j in range(i + 1, min(i + d + 1, n)):
                 if arr[j] >= arr[i]:
                     break
-                res = max(res, dfs(j) + 1)
+                res = max(res, 1 + dfs(j))
 
+            # Left
             for j in range(i - 1, max(i - d - 1, -1), -1):
                 if arr[j] >= arr[i]:
                     break
-                res = max(res, dfs(j) + 1)
-            
+                res = max(res, 1 + dfs(j))
+
+            memo[i] = res
             return res
 
         res = 1
+
         for i in range(n):
             res = max(res, dfs(i))
+
         return res
