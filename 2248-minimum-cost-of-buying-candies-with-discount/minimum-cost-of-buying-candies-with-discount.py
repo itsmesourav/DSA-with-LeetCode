68c44773b9ee95e44eedbecaff42c5264331b2b9
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-06-02 00:55:46 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (15.37%) - LeetSync

---
+++ 2248-minimum-cost-of-buying-candies-with-discount/minimum-cost-of-buying-candies-with-discount.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimumCost(self, cost: List[int]) -> int:
+        n = len(cost)
+        cost.sort()
+        res = 0
+
+        count = 1
+        for i in range(n - 1, -1, -1):
+            if count != 3:
+                res += cost[i]
+                count += 1
+            else:
+                count = 1
+        
+        return res
