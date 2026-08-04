de5b3d6f4685def2eedb6b638bdd603acdff76f0
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-08-04 23:15:18 +0530

    Time: 7 ms (52.53%) | Memory: 12.3 MB (51.38%) - LeetSync

--- 4107-find-missing-elements/find-missing-elements.py
+++ 4107-find-missing-elements/find-missing-elements.py
@@ -1,8 +1,12 @@
-class Solution:
-    def findMissingElements(self, nums: List[int]) -> List[int]:
+class Solution(object):
+    def findMissingElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
         nums = set(nums)
 
-        mn, mx = inf, -inf
+        mn, mx = float('inf'), float('-inf')
         for n in nums:
             mn = min(mn, n)
             mx = max(mx, n)
