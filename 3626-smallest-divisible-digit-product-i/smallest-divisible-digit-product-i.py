3cd42ca23328e6f09a03e53c9e963bf51cc1f8a5
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-08-07 04:35:14 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.67%) - LeetSync

--- 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.py
@@ -1,5 +1,10 @@
-class Solution:
-    def smallestNumber(self, n: int, t: int) -> int:
+class Solution(object):
+    def smallestNumber(self, n, t):
+        """
+        :type n: int
+        :type t: int
+        :rtype: int
+        """
         while True:
             p = 1
             curr = n
