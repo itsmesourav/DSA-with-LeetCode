3e5c3147c456a74c49edbbc7e950321ab99880be
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-08-16 13:10:12 +0530

    Time: 76 ms (7.14%) | Memory: 20.2 MB (42.86%) - LeetSync

--- 2156-stone-game-ix/stone-game-ix.py
+++ 2156-stone-game-ix/stone-game-ix.py
@@ -1,18 +1,12 @@
-class Solution:
-    def stoneGameIX(self, stones: List[int]) -> bool:
-        c0 = 0
-        c1 = 0
-        c2 = 0
+class Solution(object):
+    def stoneGameIX(self, stones):
+        c0, c1, c2 = 0, 0, 0
 
         for s in stones:
             r = s % 3
-            if r == 0:
-                c0 += 1
-            elif r == 1:
-                c1 += 1
-            else:
-                c2 += 1
+            if r == 0: c0 += 1
+            elif r == 1: c1 += 1
+            else: c2 += 1
         
-        if c0 % 2 == 0:
-            return c1 > 0 and c2 > 0
+        if c0 % 2 == 0: return c1 > 0 and c2 > 0
         return abs(c1 - c2) > 2
