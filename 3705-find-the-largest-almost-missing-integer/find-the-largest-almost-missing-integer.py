9800b74a67fe313303e1e81c720daff802370161
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-08-19 02:59:08 +0530

    Time: 8 ms (45.83%) | Memory: 12.4 MB (54.17%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.py
@@ -1,21 +1,20 @@
-from typing import List
-
-class Solution:
-    def largestInteger(self, nums: List[int], k: int) -> int:
-        count = {}
-
-        # Check every subarray of size k
-        for i in range(len(nums) - k + 1):
-            window = set(nums[i:i + k])
-
-            for x in window:
-                count[x] = count.get(x, 0) + 1
-
-        # Largest number appearing in exactly one subarray
-        ans = -1
-
-        for x, freq in count.items():
-            if freq == 1:
-                ans = max(ans, x)
-
-        return ans
+class Solution(object):
+    def largestInteger(self, nums, k):
+        n = len(nums)
+        if k == n:
+            return max(nums)
+        
+        fm = defaultdict(int)
+        for num in nums:
+            fm[num] += 1
+        
+        unique = []
+        for num, v in fm.items():
+            if v == 1:
+                unique.append(num)
+        
+        if k == 1:
+            return max(unique) if unique else -1
+        first = nums[0] if fm[nums[0]] == 1 else -1
+        last = nums[-1] if fm[nums[-1]] == 1 else -1
+        return max(first, last)
