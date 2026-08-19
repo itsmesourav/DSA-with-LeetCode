c8fca2d904c96da7781c4b25e6634fd76fefc8e3
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-08-20 01:29:49 +0530

    Time: 132 ms (5.13%) | Memory: 22.7 MB (77.69%) - LeetSync

---
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.py
@@ -0,0 +1,22 @@
+#from collections import defaultdict
+
+class Solution:
+    def maxNumberOfFamilies(self, n: int, reservedSeats: list[list[int]]) -> int:
+        mp = defaultdict(list)
+
+        for r, c in reservedSeats:
+            mp[r - 1].append(c - 1)
+
+        res = 0
+        seenr = 0
+
+        for k in mp:
+            seenr += 1
+
+            left = all(c not in mp[k] for c in range(1, 5))
+            middle = all(c not in mp[k] for c in range(3, 7))
+            right = all(c not in mp[k] for c in range(5, 9))
+
+            res += max(middle, left + right)
+
+        return res + (n - seenr) * 2
