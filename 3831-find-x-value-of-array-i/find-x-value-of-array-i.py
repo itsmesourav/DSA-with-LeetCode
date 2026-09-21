92bf244cfebe7760ef758015d0c27431beec8e9d
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-09-21 16:33:41 +0530

    Time: 343 ms (78.95%) | Memory: 34.1 MB (85.53%) - LeetSync

--- 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
+++ 3831-find-x-value-of-array-i/find-x-value-of-array-i.py
@@ -1,6 +1,5 @@
-class Solution(object):
-
-    def resultArray(self, nums, k):
+class Solution:
+    def resultArray(self, nums: List[int], k: int) -> List[int]:
         ans = [0] * k
         prev = [0] * k
 
