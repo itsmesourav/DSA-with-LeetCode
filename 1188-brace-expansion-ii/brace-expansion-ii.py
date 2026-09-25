fb686232b63deb9302fe517c5382679e9bd8d33b
Sourav Mahapatra <itsmesouravmahapatra@gmail.com>
2026-09-25 23:49:52 +0530

    Time: 4 ms (34.43%) | Memory: 19.6 MB (29.51%) - LeetSync

---
+++ 1188-brace-expansion-ii/brace-expansion-ii.py
@@ -0,0 +1,38 @@
+class Solution:
+    def braceExpansionII(self, expression: str) -> list[str]:
+
+        def combine(a, b):
+            result = set()
+
+            for x in a:
+                for y in b:
+                    result.add(x + y)
+
+            return result
+
+        def parse(i):
+            result = set()
+            current = {""}
+
+            while i < len(expression) and expression[i] != '}':
+                
+                if expression[i] == '{':
+                    part, i = parse(i + 1)
+                    current = combine(current, part)
+
+                elif expression[i] == ',':
+                    result.update(current)
+                    current = {""}
+                    i += 1
+
+                else:
+                    current = combine(current, {expression[i]})
+                    i += 1
+
+            result.update(current)
+
+            return result, i + 1
+
+        result, _ = parse(0)
+
+        return sorted(result)
